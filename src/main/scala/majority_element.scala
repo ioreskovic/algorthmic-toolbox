bcae70b84e502acb5091aa92539e52311043836b
eivaore <ivan.oreskovic@ericsson.com>
2017-08-02 13:47:39 +0200

    Added solution for problem 2
    Added source and submission files for problem 2

---
+++ src/main/scala/majority_element.scala
@@ -0,0 +1,37 @@
+import scala.io.StdIn
+
+object majority_element {
+  def main(args: Array[String]): Unit = {
+    StdIn.readLine()
+    val input = StdIn.readLine().split(" ").map(_.toInt)
+    val result = find(input)
+    result match {
+      case Some(_) => println(1)
+      case None => println(0)
+    }
+  }
+
+  def find[T](array: Array[T]): Option[T] = {
+    def occurrences(from: Int, to: Int, z: T): Int = {
+      array.view(from, to + 1).count(_ == z)
+    }
+
+    def loop(from: Int, to: Int): Option[T] = {
+      if (from > to) None
+      else if (from == to) Some(array(from))
+      else {
+        val mid = (from + to) / 2
+        val maybeLeft = loop(from, mid)
+        val maybeRight = loop(mid + 1, to)
+
+        (maybeLeft, maybeRight) match {
+          case (Some(x), _) if occurrences(from, to, x) > (to - from + 1) / 2 => Some(x)
+          case (_, Some(x)) if occurrences(from, to, x) > (to - from + 1) / 2 => Some(x)
+          case _ => None
+        }
+      }
+    }
+
+    loop(0, array.length - 1)
+  }
+}
