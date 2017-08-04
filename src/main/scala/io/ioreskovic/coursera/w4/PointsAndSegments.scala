fba3b6e7de35f65c993c77c714cd5dd7c6a89c13
eivaore <ivan.oreskovic@ericsson.com>
2017-08-04 15:38:06 +0200

    Solved problem #5

--- src/main/scala/io/ioreskovic/coursera/w4/PointsAndSegments.scala
+++ src/main/scala/io/ioreskovic/coursera/w4/PointsAndSegments.scala
@@ -1,6 +1,5 @@
 package io.ioreskovic.coursera.w4
 
-
 import scala.annotation.tailrec
 import scala.io.StdIn
 
@@ -10,7 +9,7 @@ object PointsAndSegments {
     def priority: Int
   }
 
-  case class QueryPoint(value: Int) extends Point {
+  case class QueryPoint(value: Int, originalIndex: Int) extends Point {
     override def priority: Int = 1
   }
 
@@ -22,6 +21,8 @@ object PointsAndSegments {
     override def priority: Int = 2
   }
 
+  case class Result(inclusions: Int, originalIndex: Int)
+
   implicit val pointOrdering: Ordering[Point] = new Ordering[Point] {
     override def compare(x: Point, y: Point): Int = {
       val cmp = x.value.compareTo(y.value)
@@ -30,20 +31,36 @@ object PointsAndSegments {
     }
   }
 
-  type Segment = (SegmentStart, SegmentEnd)
-
-  def parseSegment(s: String): Segment = {
+  def parseSegment(s: String): List[Point] = {
     val info = s.split(" ")
-    (SegmentStart(info(0).toInt), SegmentEnd(info(1).toInt))
+    List(SegmentStart(info(0).toInt), SegmentEnd(info(1).toInt))
   }
 
-  def parseQueryPoints(s: String): List[QueryPoint] = {
-    s.split(" ").map(e => QueryPoint(e.toInt)).toList
+  def parseQueryPoints(s: String): List[Point] = {
+    s.split(" ").zipWithIndex.map{ case (e, i) => QueryPoint(e.toInt, i) }.toList
   }
 
   def main(args: Array[String]): Unit = {
     val metaInfo = StdIn.readLine().split(" ")
     val nSegments = metaInfo(0).toInt
     val nPoints = metaInfo(1).toInt
+
+    val segments = (0 until nSegments).flatMap(_ => parseSegment(StdIn.readLine())).toList
+    val queryPoints = parseQueryPoints(StdIn.readLine())
+
+    val points = (segments ::: queryPoints).sorted(pointOrdering)
+
+    val result = solve(points, Nil, 0, 0).sortBy(_.originalIndex)
+    val outputs = result.map(_.inclusions)
+
+    println(outputs.mkString(" "))
+  }
+
+  @tailrec
+  def solve(px: List[Point], res: List[Result], starts: Int, ends: Int): List[Result] = px match {
+    case SegmentStart(_) :: ps => solve(ps, res, starts + 1, ends)
+    case SegmentEnd(_) :: ps => solve(ps, res, starts, ends + 1)
+    case QueryPoint(_, idx) :: ps => solve(ps, Result(starts - ends, idx) :: res, starts, ends)
+    case _ => res
   }
 }
--- src/main/scala/points_and_segments.scala
+++ src/main/scala/points_and_segments.scala
@@ -2,75 +2,40 @@ import scala.annotation.tailrec
 import scala.io.StdIn
 
 object points_and_segments {
-  case class Point(value: Int, index: Int) {
-    def isBefore(s: Segment): Boolean = {
-      value < s.from
-    }
-
-    def isAfter(s: Segment): Boolean = {
-      value > s.to
-    }
-
-    def isInside(s: Segment): Boolean = {
-      value >= s.from && value <= s.to
-    }
+  trait Point {
+    def value: Int
+    def priority: Int
   }
 
-  case class Segment(from: Int, to: Int)
-
-  case class PointResult(p: Point, result: Int = 0) {
-    def incrementBy(value: Int): PointResult = PointResult(p, result + value)
+  case class QueryPoint(value: Int, originalIndex: Int) extends Point {
+    override def priority: Int = 1
   }
 
-  type Points = List[Point]
-  type Segments = List[Segment]
-
-  implicit val segmentOrdering: Ordering[Segment] = new Ordering[Segment] {
-    def compare(x: Segment, y: Segment): Int = {
-      val cmpFrom = x.from.compareTo(y.from)
-      if (cmpFrom == 0) cmpFrom
-      else x.to.compareTo(y.to)
-    }
+  case class SegmentStart(value: Int) extends Point {
+    override def priority: Int = 0
   }
+
+  case class SegmentEnd(value: Int) extends Point {
+    override def priority: Int = 2
+  }
+
+  case class Result(inclusions: Int, originalIndex: Int)
 
   implicit val pointOrdering: Ordering[Point] = new Ordering[Point] {
-    def compare(x: Point, y: Point): Int = {
-      x.value.compare(y.value)
+    override def compare(x: Point, y: Point): Int = {
+      val cmp = x.value.compareTo(y.value)
+      if (cmp != 0) cmp
+      else x.priority.compareTo(y.priority)
     }
   }
 
-  implicit def parseSegment(s: String): Segment = {
+  def parseSegment(s: String): List[Point] = {
     val info = s.split(" ")
-    Segment(info(0).toInt, info(1).toInt)
+    List(SegmentStart(info(0).toInt), SegmentEnd(info(1).toInt))
   }
 
-  implicit def parsePoint(s: String, index: Int): Point = {
-    Point(s.toInt, index)
-  }
-
-  def scan(points: Points, segments: Segments): Seq[PointResult] = {
-    @tailrec
-    def loop(px: Points, sx: Segments, res: List[PointResult]): List[PointResult] = (px, sx, res) match {
-      case (p :: ps, s :: ss, Nil) if p.isInside(s) => loop(px, sx, PointResult(p) :: res)
-      case (p :: ps, s :: ss, Nil) if p.isBefore(s) => loop(ps, sx, PointResult(p) :: res)
-      case (p :: ps, s :: ss, Nil) if p.isAfter(s) => loop(px, ss, PointResult(p) :: res)
-
-      case (p :: ps, s :: ss, r :: rs) if r.p == p && p.isInside(s) => loop(px, ss, r.incrementBy(1) :: rs)
-      case (p :: ps, s :: ss, r :: rs) if r.p == p && p.isBefore(s) => loop(ps, sx, res)
-      case (p :: ps, s :: ss, r :: rs) if r.p == p && p.isAfter(s) => loop(px, ss, res)
-
-      case (p :: ps, s :: ss, r :: rs) if r.p != p && p.isInside(s) => loop(px, sx, PointResult(p) :: res)
-      case (p :: ps, s :: ss, r :: rs) if r.p != p && p.isBefore(s) => loop(ps, sx, PointResult(p) :: res)
-      case (p :: ps, s :: ss, r :: rs) if r.p != p && p.isAfter(s) => loop(px, ss, PointResult(p) :: res)
-
-      case (p :: ps, Nil, Nil) => loop(ps, sx, PointResult(p) :: res)
-      case (p :: ps, Nil, r :: rs) if r.p == p => loop(ps, sx, res)
-      case (p :: ps, Nil, r :: rs) => loop(ps, sx, PointResult(p) :: res)
-      case _ => res
-    }
-
-    val sortedPoints = points.sorted(pointOrdering)
-    loop(sortedPoints, segments.sorted(segmentOrdering), Nil).reverse
+  def parseQueryPoints(s: String): List[Point] = {
+    s.split(" ").zipWithIndex.map{ case (e, i) => QueryPoint(e.toInt, i) }.toList
   }
 
   def main(args: Array[String]): Unit = {
@@ -78,10 +43,22 @@ object points_and_segments {
     val nSegments = metaInfo(0).toInt
     val nPoints = metaInfo(1).toInt
 
-    val segments = (0 until nSegments).map(_ => parseSegment(StdIn.readLine())).toList
-    val points = StdIn.readLine().split(" ").zipWithIndex.map(p => parsePoint(p._1, p._2)).toList
+    val segments = (0 until nSegments).flatMap(_ => parseSegment(StdIn.readLine())).toList
+    val queryPoints = parseQueryPoints(StdIn.readLine())
 
-    val result = scan(points, segments).sortBy(_.p.index).map(_.result)
-    println(result.mkString(" "))
+    val points = (segments ::: queryPoints).sorted(pointOrdering)
+
+    val result = solve(points, Nil, 0, 0).sortBy(_.originalIndex)
+    val outputs = result.map(_.inclusions)
+
+    println(outputs.mkString(" "))
+  }
+
+  @tailrec
+  def solve(px: List[Point], res: List[Result], starts: Int, ends: Int): List[Result] = px match {
+    case SegmentStart(_) :: ps => solve(ps, res, starts + 1, ends)
+    case SegmentEnd(_) :: ps => solve(ps, res, starts, ends + 1)
+    case QueryPoint(_, idx) :: ps => solve(ps, Result(starts - ends, idx) :: res, starts, ends)
+    case _ => res
   }
 }
